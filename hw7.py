32779ae4341329df825cebb3129fbdaea3889df5
islam1221 <88580339+islam1221@users.noreply.github.com>
2021-08-24 19:51:46 +0600

    Update hw7.py

--- hw7.py
+++ hw7.py
@@ -3,17 +3,18 @@ import random
 import datetime
 
 number = random.randint(1, 100)
-count = 0
 print("instruction:\n"
       "very hot +-5\n"
       "hot +-10\n"
       "cold +- 25"
       "very cold +-100")
+count = int(input('put your attempts:'))
 print("Let's go!\n"
       "Put your number:")
+
 with open('text.txt1', 'a') as a:
     sec = datetime.datetime.now()
-    while 1:
+    while count != 0:
         try:
             guess = int(input(''))
         except ValueError:
@@ -21,25 +22,25 @@ with open('text.txt1', 'a') as a:
             continue
 
         if guess <= number + 5 and guess >= number - 5:
-            count += 1
+            count -= 1
             if guess < number:
                 print("it's very hot, bigger")
             elif guess > number:
                 print("it's very hot, lower")
         elif guess <= number + 10 and guess >= number - 10:
-            count += 1
+            count -= 1
             if guess < number:
                 print("it's hot, bigger")
             elif guess > number:
                 print("it's hot, lower")
         elif guess <= number + 25 and guess >= number - 25:
-            count += 1
+            count -= 1
             if guess < number:
                 print("it's cold, bigger")
             elif guess > number:
                 print("it's cold, lower")
         elif guess < (number + 25) and guess > 0 or guess > (number + 25) and guess <= 100:
-            count += 1
+            count -= 1
             if guess < number:
                 print("it's very cold, bigger")
             elif guess > number:
@@ -59,3 +60,8 @@ with open('text.txt1', 'a') as a:
             a.write(f"You found number for {count} attempts\n")
             a.write(f"Time {sec1.seconds}\n")
             break
+        if count == 0:
+            print(f'End game;(\n'
+                    f"Your number was {number}\n"
+                  f"In future will be better:)")
+            break         
