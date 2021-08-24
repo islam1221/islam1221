af6f12c092a905388ba2d38bfdf705ea6aafc004
islam1221 <88580339+islam1221@users.noreply.github.com>
2021-08-24 14:31:52 +0600

    Update hw7.py

--- hw7.py
+++ hw7.py
@@ -1,1 +1,62 @@
-import randomimport datetimenumber = random.randint(1, 100)count = 0print("instruction:\n"      "very hot +-5\n"      "hot +-10\n"      "cold +- 25"      "very cold +-100")print("Let's go!\n"      "Put your number:")with open('text.txt1', 'a') as a:    sec = datetime.datetime.now()    while 1:        try:            guess = int(input(''))        except ValueError:            print("Put only numbers")            continue        if guess <= number + 5 and guess >= number - 5:            count += 1            if guess < number:                print("it's very hot, bigger")            elif guess > number:                print("it's very hot, lower")        elif guess <= number + 10 and guess >= number - 10:            count += 1            if guess < number:                print("it's hot, bigger")            elif guess > number:                print("it's hot, lower")        elif guess <= number + 25 and guess >= number - 25:            count += 1            if guess < number:                print("it's cold, bigger")            elif guess > number:                print("it's cold, lower")        elif guess < (number + 25) and guess > 0 or guess > (number + 25) and guess <= 100:            count += 1            if guess < number:                print("it's very cold, bigger")            elif guess > number:                print("it's very cold, lower")        else:            print('Print namber into range')        if guess == 0:            print("Exit programm\n\n\n"                "Made by Islamchik04")            break        if guess == number:            count += 1            print(f"You win from {count} time \nThanks for playing my game!\n")            print(f"Guessed in {count} attempts!")            sec1 = datetime.datetime.now() - sec            a.write(f"You started in: {sec.strftime('%X')} \n")            a.write(f"You finished in: {datetime.datetime.now().strftime('%X')} \n")            a.write(f"You found number for {count} attempts\n")            a.write(f"Time {sec1.seconds}\n")            break
+
+import random
+import datetime
+
+number = random.randint(1, 100)
+count = 0
+print("instruction:\n"
+      "very hot +-5\n"
+      "hot +-10\n"
+      "cold +- 25"
+      "very cold +-100")
+print("Let's go!\n"
+      "Put your number:")
+with open('text.txt1', 'a') as a:
+    sec = datetime.datetime.now()
+    while 1:
+        try:
+            guess = int(input(''))
+        except ValueError:
+            print("Put only numbers")
+            continue
+
+        if guess <= number + 5 and guess >= number - 5:
+            count += 1
+            if guess < number:
+                print("it's very hot, bigger")
+            elif guess > number:
+                print("it's very hot, lower")
+        elif guess <= number + 10 and guess >= number - 10:
+            count += 1
+            if guess < number:
+                print("it's hot, bigger")
+            elif guess > number:
+                print("it's hot, lower")
+        elif guess <= number + 25 and guess >= number - 25:
+            count += 1
+            if guess < number:
+                print("it's cold, bigger")
+            elif guess > number:
+                print("it's cold, lower")
+        elif guess < (number + 25) and guess > 0 or guess > (number + 25) and guess <= 100:
+            count += 1
+            if guess < number:
+                print("it's very cold, bigger")
+            elif guess > number:
+                print("it's very cold, lower")
+        else:
+            print('Print namber into range')
+        if guess == 0:
+            print("Exit programm\n\n\n"
+                "Made by Islamchik04")
+            break
+        if guess == number:
+            count += 1
+            print(f"You win from {count} time \nThanks for playing my game!\n")
+            print(f"Guessed in {count} attempts!")
+            sec1 = datetime.datetime.now() - sec
+            a.write(f"You started in: {sec.strftime('%X')} \n")
+            a.write(f"You finished in: {datetime.datetime.now().strftime('%X')} \n")
+            a.write(f"You found number for {count} attempts\n")
+            a.write(f"Time {sec1.seconds}\n")
+            break
