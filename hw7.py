644394419e21f3ae2811da1feea083c8fbab4152
islam1221 <88580339+islam1221@users.noreply.github.com>
2021-08-24 21:43:59 +0600

    Update hw7.py

--- hw7.py
+++ hw7.py
@@ -56,8 +56,8 @@ with open('text.txt1', 'a') as a:
                 "Made by Islamchik04")
             break
         if guess == number:
-            print(f"You win from {count} time \nThanks for playing my game!\n")
-            print(f"Guessed in {count} attempts!")
+            print(f"You win from {v} time \nThanks for playing my game!\n")
+            print(f"Guessed in {v} attempts!")
             sec1 = datetime.datetime.now() - sec
             a.write(f"You started in: {sec.strftime('%X')} \n")
             a.write(f"You finished in: {datetime.datetime.now().strftime('%X')} \n")
